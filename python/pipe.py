2aaf1b320ac3556980f6d1b4d51b34d86ed9be7b
Nigthwolf98 <72725246+Nigthwolf98@users.noreply.github.com>
2023-09-21 00:03:59 -0300

    Formateo de codigo

--- python/pipe.py
+++ python/pipe.py
@@ -6,11 +6,11 @@ def prompt(pipe):
         text = str(input())
         pipe.send(text.lower())
 
-def childLabour(pipe):
+def child_labour(pipe):
     longest = ""
     shortest = ""
-    shortFlag = False
-    totalChar = 0
+    short_flag = False
+    total_char = 0
     letters = 0
     digits = 0
 
@@ -18,24 +18,24 @@ def childLabour(pipe):
         data = pipe.recv()
         if data == 'close':
             break
-        else:
-            dataLength = len(data)
-            totalChar += dataLength
-            if dataLength > len(longest):
-                longest = data
-            
-            if dataLength < len(shortest) or not(shortFlag) :
-                shortest = data
-                shortFlag = True
-            
-            letters += sum(1 for l in data if l.isalpha())
-            digits += sum(1 for n in data if n.isdigit())
 
-    child.send(totalChar)
-    child.send(letters)
-    child.send(digits)
-    child.send(longest)
-    child.send(shortest)
+        data_length = len(data)
+        total_char += data_length
+        if data_length > len(longest):
+            longest = data
+
+        if data_length < len(shortest) or not short_flag:
+            shortest = data
+            short_flag = True
+
+        letters += sum(1 for l in data if l.isalpha())
+        digits += sum(1 for n in data if n.isdigit())
+
+    pipe.send(total_char)
+    pipe.send(letters)
+    pipe.send(digits)
+    pipe.send(longest)
+    pipe.send(shortest)
 
 def statistics(pipe):
     print("Total de caracteres: " + str(pipe.recv()))
@@ -45,12 +45,10 @@ def statistics(pipe):
     print("Palabra mas corta: " + pipe.recv())
 
 if __name__ == '__main__':
-    parent, child = Pipe(duplex=True)
+    PARENT, CHILD = Pipe(duplex=True)
 
-    child_process = Process(target=childLabour, args=(child,))
+    child_process = Process(target=child_labour, args=(CHILD,))
     child_process.start()
-    prompt(parent)
-    statistics(parent)
+    prompt(PARENT)
+    statistics(PARENT)
     child_process.join()
-
-    
